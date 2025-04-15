fd396f1e414c574d80842a2ed5ea5ec4a01dee8d
1123581321345589144233377610987 <123220105+1123581321345589144233377610987@users.noreply.github.com>
2025-04-15 17:57:57 -0600

    Update rsh.c

--- rsh.c
+++ rsh.c
@@ -179,12 +179,7 @@ int main() {
         }
 		// Wait for the spawned process to terminate
 		if(index<9){
-			if (waitpid(pid, &status, 0) == -1) {
-				perror("waitpid failed");
-				exit(EXIT_FAILURE);
-			}
-		
-		
+
 			// Destroy spawn attributes
 			posix_spawnattr_destroy(&attr);
 		}
